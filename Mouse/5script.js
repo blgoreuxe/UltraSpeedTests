3e2436ed449e183995ba3df5c42d0498f36cf9c3
blgoreuxe <90001868+blgoreuxe@users.noreply.github.com>
2022-03-29 10:17:53 -0400

    Add files via upload

---
+++ Mouse/5script.js
@@ -0,0 +1,29 @@
+let score = 0;
+let game = false;
+let time = 5;
+
+function tap() {
+    if (game == false) {
+        game = true;
+        time = 5;
+        document.getElementById("time").textContent = "Time Left: " + time
+        score = 0;
+        document.getElementById("score").textContent = "Score: " + score
+        score += 1;
+        document.getElementById("score").textContent = "Score: " + score
+        inverval_timer = setInterval(function() { 
+            time -= 1;
+            document.getElementById("time").textContent = "Time Left: " + time
+            if (time == 0) {
+                clearInterval(inverval_timer);
+                let cps = score/5
+                alert("You got a score of " + score + "\nYour CPS is " + cps)
+                game = false;
+                document.getElementById("prompt").style.visibility = "visible"
+            }
+        }, 1000);
+    } else {
+        score += 1;
+        document.getElementById("score").textContent = "Score: " + score
+    }
+}
