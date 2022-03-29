39d42ff2aa4fd9cd5c5cdd6a2fea8e74be88bf0c
blgoreuxe <90001868+blgoreuxe@users.noreply.github.com>
2022-03-29 14:15:09 -0400

    Add files via upload

---
+++ Spacebar/5script.js
@@ -0,0 +1,41 @@
+let score = 0;
+let game = false;
+let time = 5;
+let shouldHandleKeyDown = true;
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode == 32) {
+        if (game == false) {
+            if (!shouldHandleKeyDown) return;
+            shouldHandleKeyDown = false;
+            game = true;
+            document.getElementById("prompt").style.visibility = "hidden"
+            time = 5;
+            document.getElementById("time").textContent = "Time Left: " + time
+            score = 0;
+            document.getElementById("score").textContent = "Score: " + score
+            score += 1;
+            document.getElementById("score").textContent = "Score: " + score
+            inverval_timer = setInterval(function() { 
+                time -= 1;
+                document.getElementById("time").textContent = "Time Left: " + time
+                if (time == 0) {
+                    clearInterval(inverval_timer);
+                    let cps = score/5
+                    alert("You got a score of " + score + "\nYour CPS is " + cps)
+                    game = false;
+                    document.getElementById("prompt").style.visibility = "visible"
+                }
+            }, 1000);
+        } else {
+            if (!shouldHandleKeyDown) return;
+            shouldHandleKeyDown = false;
+            score += 1;
+            document.getElementById("score").textContent = "Score: " + score
+        }
+    }
+});
+
+document.addEventListener('keyup', function(event){
+    shouldHandleKeyDown = true;
+});
